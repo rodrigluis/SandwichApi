9b51a1675b709075c8337f579a9cccffc9b2225a
Duser <duser.abis@aze.be>
2024-02-07 16:52:24 +0100

    Bug fix endpoint naming Add exceptions

--- src/main/java/be/abis/sandwich/controller/SandwichApiController.java
+++ src/main/java/be/abis/sandwich/controller/SandwichApiController.java
@@ -8,49 +8,47 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("sandwich")
 public class SandwichApiController {
     @Autowired
     private SandwichService sandwichService;
 
-    @PostMapping(path = "", consumes = {MediaType.APPLICATION_JSON_VALUE})
+    @PostMapping(path = "/sandwich", consumes = {MediaType.APPLICATION_JSON_VALUE})
     @RolesAllowed("AbisAdmin")
     public void addSandwich(@Valid @RequestBody Sandwich sandwich) {
         sandwichService.addSandwich(sandwich);
     }
 
-    @PatchMapping(path = "", consumes = {MediaType.APPLICATION_JSON_VALUE})
+    @PatchMapping(path = "/sandwich", consumes = {MediaType.APPLICATION_JSON_VALUE})
     @RolesAllowed("AbisAdmin")
     public void updateSandwichPrice(Sandwich sandwich) {
         sandwichService.updatePrice(sandwich);
     }
 
-    @DeleteMapping(path = "{id}")
+    @DeleteMapping(path = "/sandwich/{id}")
     @RolesAllowed("AbisAdmin")
     public void deleteSandwich(@PathVariable("id") int id) {
         sandwichService.deleteSandwich(id);
     }
 
-    @GetMapping
+    @GetMapping(path = "/sandwiches/all")
     public List<Sandwich> getAllSandwiches() {
         return sandwichService.findAllSandwiches();
     }
 
-    @GetMapping(path = "{id}")
+    @GetMapping(path = "/sandwich/{id}")
     public Sandwich getSandwichById(@PathVariable("id") int id) {
         return sandwichService.findSandwichById(id);
     }
 
-    @GetMapping
+    @GetMapping(path = "/sandwich")
     public Sandwich getSandwichByName(@RequestParam("name") String name) {
         return sandwichService.findSandwichByName(name);
     }
 
-    @GetMapping
+    @GetMapping(path = "/sandwiches")
     public List<Sandwich> getAllSandwichesByCategory(@RequestParam("category") String category) {
         return sandwichService.findSandwichesByCategory(category);
     }
--- src/main/java/be/abis/sandwich/repository/JdbcSandwichRepository.java
+++ src/main/java/be/abis/sandwich/repository/JdbcSandwichRepository.java
@@ -1,5 +1,6 @@
 package be.abis.sandwich.repository;
 
+import be.abis.sandwich.exception.ApiException;
 import be.abis.sandwich.model.Sandwich;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Repository;
@@ -17,37 +18,54 @@ public class JdbcSandwichRepository implements SandwichRepository {
     public List<Sandwich> findAllSandwiches() {
         List<Sandwich> sandwiches = new ArrayList<>();
         sandwichRepository.findAll().forEach(sandwiches::add);
+        if (sandwiches.size() <= 0) {
+            throw new ApiException(ApiException.Type.NO_SANDWICH);
+        }
         return sandwiches;
     }
 
     @Override
     public Sandwich findSandwichById(int id) {
         Optional<Sandwich> sandwich = sandwichRepository.findById(Integer.valueOf(id));
-        return (sandwich.isPresent()?sandwich.get():null);
+        if (! sandwich.isPresent()) {
+            throw new ApiException(ApiException.Type.DOES_NOT_EXIST);
+        }
+        return sandwich.get();
     }
 
     @Override
     public Sandwich findSandwichByName(String name) {
-        return sandwichRepository.findSandwichByName(name);
+        Sandwich sandwich = sandwichRepository.findSandwichByName(name);
+        if (sandwich == null) {
+            throw new ApiException(ApiException.Type.DOES_NOT_EXIST);
+        }
+        return sandwich;
     }
 
     @Override
     public List<Sandwich> findSandwichesByCategory(String category) {
-        return sandwichRepository.findSandwichesByCategory(category);
+        List<Sandwich> sandwiches = sandwichRepository.findSandwichesByCategory(category);
+        if (sandwiches.size() <= 0) {
+            throw new ApiException(ApiException.Type.DOES_NOT_EXIST);
+        }
+        return sandwiches;
     }
 
     @Override
     public void addSandwich(Sandwich sandwich) {
+        // Check to see whether thesandwich already exists
         sandwichRepository.save(sandwich);
     }
 
     @Override
     public void updatePrice(Sandwich sandwich) {
+        // Check to see whether thesandwich already exists
         sandwichRepository.save(sandwich);
     }
 
     @Override
     public void deleteSandwich(int id) {
+        // Check to see whether thesandwich already exists
         sandwichRepository.deleteById(Integer.valueOf(id));
     }
 }
